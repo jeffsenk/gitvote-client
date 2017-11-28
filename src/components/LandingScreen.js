dd35d001c0faa123b160688fa2bb323f15d6882b
jeffsenk <jeff.senk@gmail.com>
2017-11-28 17:42:52 -0500

    sketch for landing page

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Navbar,Button,FormControl,FormGroup} from 'react-bootstrap';
 
 export default class LandingScreen extends React.Component{
 
   render(){
     return(
       <div>
-        <div>Landing Page</div>
+        <Navbar>
+          <Navbar.Form pullRight>
+            <FormGroup>
+              <FormControl style={{width:'175px'}} type='text' placeholder='Email'/>
+              <FormControl style={{width:'175px'}} type='text' placeholder='Password'/>
+            </FormGroup>
+            <Button type='submit'>Submit</Button>
+          </Navbar.Form>
+        </Navbar>
+      <div style={{display:'flex',flexDirection:'column',alignItems:'center',marginTop:'10%'}}>
+        <h1>GitVote</h1>
+        <Button bsStyle='success' style={{marginTop:'3%'}}>Sign Up</Button>
+      </div>
       </div>
     );
   }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,7 +4,8 @@ import ProposalScreen from './ProposalScreen';
 import MemberScreen from './MemberScreen';
 import NewProposalForm from './NewProposalForm';
 import ProposalDetail from './ProposalDetail';
-import {Nav, NavItem} from 'react-bootstrap';
+import AdminScreen from './AdminScreen';
+import {Nav, NavItem,Navbar} from 'react-bootstrap';
 
 export default class MainContainer extends React.Component{
 
@@ -15,14 +16,23 @@ export default class MainContainer extends React.Component{
 
     return(
       <div style={main}>
+        <Navbar>
+        <Navbar.Header>
+        <Navbar.Brand>
+          <a href='/'>GitVote</a>
+        </Navbar.Brand>
+        </Navbar.Header>
         <Nav bsStyle='tabs'>
           <NavItem href={this.props.match.url + '/proposals'}>Proposals</NavItem>
           <NavItem href={this.props.match.url + '/members'}>Members</NavItem>
+          <NavItem href={this.props.match.url + '/admin'}>Admin</NavItem>
         </Nav>
+        </Navbar>
         <Switch>
           <Redirect from={this.props.match.url} exact to={this.props.match.url+'/proposals'}/>
           <Route exact path={this.props.match.url+'/proposals'} component={ProposalScreen}/>
           <Route exact path={this.props.match.url+'/members'} component={MemberScreen}/>
+          <Route exact path={this.props.match.url+'/admin'} component={AdminScreen}/>
           <Route exact path={this.props.match.url+'/proposals/new'} component={NewProposalForm}/>
           <Route exact path={this.props.match.url+'/proposals/:id'} component={ProposalDetail}/>
         </Switch>
