9a2a8457f024a706583274fa7fdbbddcc131bc16
jeffsenk <jeff.senk@gmail.com>
2017-12-14 14:43:31 -0600

    added functionality to accept / decline invitation

--- src/components/GroupScreen.js
+++ src/components/GroupScreen.js
@@ -11,13 +11,72 @@ export default class GroupScreen extends React.Component{
       invitations:{}
     }
     this.handleSignOut = this.handleSignOut.bind(this);
+    this.fetchInvitations = this.fetchInvitations.bind(this);
+    this.acceptInvitation = this.acceptInvitation.bind(this);
+    this.declineInvitation = this.declineInvitation.bind(this);
   }
   handleSignOut(){
     this.props.auth.signOut();
   }
 
-  componentDidMount(){
-    if(this.props.auth.currentUser){
+  acceptInvitation(teamKey,userKey){
+    fetch('/acceptInvitation',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+       },
+      body: JSON.stringify({
+        userKey:userKey,
+        teamKey:teamKey
+      })
+    }).then(function(res){
+      if(!res.ok){
+        alert('Error accepting');
+      }else{
+        this.fetchTeams();
+        this.fetchInvitations();
+      }
+    }.bind(this));
+  }
+
+  declineInvitation(teamKey,userKey){
+    fetch('/declineInvitation',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+       },
+      body: JSON.stringify({
+        userKey:userKey,
+        teamKey:teamKey
+      })
+    }).then(function(res){
+      if(!res.ok){
+        alert('Error declining');
+      }else{
+        this.fetchTeams();
+        this.fetchInvitations();
+      }
+    }.bind(this));
+  }
+
+  fetchInvitations(){
+    fetch('/userInvitations',{
+      method:'POST',
+      headers:{
+	'Accept': 'application/json',
+	'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+	userKey: this.props.auth.currentUser.uid
+      })
+    }).then((res)=>res.json()).then((invitations)=>{
+      this.setState({invitations:invitations});
+    })
+  }
+
+  fetchTeams(){
       fetch('/userTeams',{
         method:'POST',
         headers:{
@@ -28,20 +87,14 @@ export default class GroupScreen extends React.Component{
           userKey: this.props.auth.currentUser.uid
         })
       }).then((res)=>res.json()).then((teams)=>{
-	this.setState({teams:teams});
-      }) 
-      fetch('/userInvitations',{
-        method:'POST',
-        headers:{
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userKey: this.props.auth.currentUser.uid
-        })
-      }).then((res)=>res.json()).then((invitations)=>{
-        this.setState({invitations:invitations});
-      })   
+        this.setState({teams:teams});
+      })
+  }
+
+  componentDidMount(){
+    if(this.props.auth.currentUser){
+      this.fetchTeams();
+      this.fetchInvitations(); 
     }
   }
 
@@ -52,14 +105,14 @@ export default class GroupScreen extends React.Component{
     let teamArray =[];
     if(this.state.teams!=null){
       for(var key in this.state.teams){
-        teamArray.push(<TeamItem {...this.props} key={key} teamKey={key} name={this.state.teams[key].name}/>);
+        teamArray.push(<TeamItem {...this.props} key={key} invitation={false} userKey={this.props.auth.currentUser.uid} teamKey={key} name={this.state.teams[key].name}/>);
       }  
     }
     teamArray.push(<ListGroupItem key='new' href='/newteam'> + Create a New Team </ListGroupItem>);
     let invitationArray =[];
     if(this.state.invitations!=null){
       for(key in this.state.invitations){
-        invitationArray.push(<TeamItem {...this.props} key={key} teamKey={key} name={this.state.invitations[key].name}/>);
+        invitationArray.push(<TeamItem {...this.props} accept={this.acceptInvitation} decline={this.declineInvitation} key={key} invitation={true} userKey={this.props.auth.currentUser.uid} teamKey={key} name={this.state.invitations[key].name}/>);
       }
     }
     return(
--- src/components/TeamItem.js
+++ src/components/TeamItem.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import TeamIcon from 'react-icons/lib/fa/rocket';
+import {Button} from 'react-bootstrap';
 
 export default class TeamItem extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      highlight:'initial'
+      highlight:'initial',
     }
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
     this.handleMouseExit = this.handleMouseExit.bind(this);
+    this.handleAccept = this.handleAccept.bind(this);
+    this.handleDecline = this.handleDecline.bind(this);
+  }
+
+  handleAccept(){
+     this.props.accept(this.props.teamKey,this.props.userKey);
+  }
+
+  handleDecline(){
+    this.props.decline(this.props.teamKey,this.props.userKey);
   }
 
   handleMouseEnter(){
@@ -38,6 +49,24 @@ export default class TeamItem extends React.Component{
       fontSize:'large'
     }
 
+    if(this.props.invitation){
+      return(
+      <div style={main} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseExit}>
+	<TeamIcon style={{marginLeft:'2%',height:'30px',width:'30px'}}/>
+	<div style={title}><Link to={{pathname:this.props.match.url+'/'+this.props.teamKey}}>
+	{this.props.name}</Link>
+	</div>
+        <div style={{marginLeft:'50%',display:'flex',flexDirection:'row'}}>
+          <div>
+            <Button bsStyle='success' onClick={this.handleAccept}>Accept</Button>
+          </div>
+          <div style={{marginLeft:'5%'}}>
+            <Button bsStyle='danger' onClick={this.handleDecline}>Decline</Button>
+          </div>
+        </div>
+      </div>
+      )
+    }
     return(
       <div style={main} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseExit}>
 	<TeamIcon style={{marginLeft:'2%',height:'30px',width:'30px'}}/>
